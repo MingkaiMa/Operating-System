6a76bf84646d5d259fdc6d1149eff22f8041c5fe
Mingkai Ma <mamingkai824@gmail.com>
2018-03-14 16:17:28 +1100

    process and thread

---
+++ Process&Thread/Semaphore-Producer_Consumer.c
@@ -0,0 +1,35 @@
+#define N 100
+typedef int semaphore;
+semaphore mutex = 1;
+semaphore empty = N;
+semaphore full = 0;
+
+void producer(void)
+{
+    int item;
+
+    while(TRUE)
+    {
+        item = produce_item();
+        down(&empty);
+        down(&mutex);
+        insert_item(item);
+        up(&mutex);
+        up(&full);
+    }
+}
+
+void consumer(void)
+{
+    int item;
+
+    while(TRUE)
+    {
+        down(&full);
+        down(&mutex);
+        item = remove_item();
+        up(&mutex);
+        up(&empty);
+        consume_item(item);
+    }
+}
